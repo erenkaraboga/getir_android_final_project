6e1d21ab7d61e0c489cff9b02d50d15d13258ac5
erenkaraboga <eren.karaboga@naylalabs.com>
2024-04-11 23:16:32 +0300

    chore:integrate Dagger Hilt into project modules

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -40,4 +42,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
 }
