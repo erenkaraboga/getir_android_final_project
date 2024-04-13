bfc6253635fd0c6585b57c77bfb915e30710a4e4
erenkaraboga <eren.karaboga@naylalabs.com>
2024-04-13 13:45:31 +0300

    feature : add Product Use Case and Implementation for connecting with the domain and data layers.

---
+++ core/src/main/java/com/getir/core/domain/usecases/home/GetPopularMoviesUseCaseImpl.kt
@@ -0,0 +1,40 @@
+package com.getir.core.domain.usecases.home
+
+
+import com.getir.core.R
+import com.getir.core.common.utils.UiText
+import com.getir.core.common.extentions.handleError
+import com.getir.core.common.utils.Resource
+import com.getir.core.domain.mappers.ProductMapper
+import com.getir.core.domain.models.Product
+import com.getir.core.domain.repositories.ProductRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+class GetProductsUseCaseImpl @Inject constructor(
+    private val repository: ProductRepository,
+
+    private val mapper: ProductMapper
+) :
+    GetProductsUseCase {
+    override suspend fun getProducts(): Flow<Resource<List<Product>>> = flow {
+        try {
+            emit(Resource.Loading())
+
+            val response = repository.getProducts().first().products
+            emit(Resource.Success(data = response.map(mapper::fromDtoToDomain)))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.handleError()))
+        } catch (e: IOException) {
+            emit(Resource.Error(UiText.StringResource(R.string.couldntReachServerError)))
+        }
+    }
+
+    companion object {
+        val CACHE_EXPIRATION_TIME = TimeUnit.MINUTES.toMillis(5)
+    }
+}
