2e4f3092d51b190907abe4964468c8d0a0840b45
erenkaraboga <eren.karaboga@naylalabs.com>
2024-04-13 14:15:32 +0300

    feat : add app module for provide gson and Product Mapper

---
+++ core/src/main/java/com/getir/core/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.getir.core.di
+
+
+import com.google.gson.Gson
+
+import com.getir.core.domain.mappers.ProductMapper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideGson() = Gson()
+
+
+
+    @Provides
+    @Singleton
+    fun provideProductMapper(): ProductMapper = ProductMapper()
+
+
+}
