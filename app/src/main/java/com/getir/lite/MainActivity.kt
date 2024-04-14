d652522c482ef396fe7b89159f21a42903310c03
erenkaraboga <eren.karaboga@naylalabs.com>
2024-04-14 16:35:57 +0300

    feat : update main activity for listen tool bar change state

--- app/src/main/java/com/getir/lite/MainActivity.kt
+++ app/src/main/java/com/getir/lite/MainActivity.kt
@@ -2,10 +2,12 @@ package com.getir.lite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.core.net.toUri
 import androidx.navigation.NavController
 import androidx.navigation.NavDeepLinkRequest
 import androidx.navigation.fragment.NavHostFragment
+import com.getir.core.SharedViewModel
 import com.getir.lite.databinding.ActivityMainBinding
 
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,14 +16,25 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+    private val sharedViewModel: SharedViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setListeners()
         setUpNavigation()
+        setUpObservers()
     }
 
+    private fun setUpObservers() {
+        sharedViewModel.toolBarType.observe(this) {
+            binding.customToolBar.setType(it)
+        }
+        sharedViewModel.cartAmount.observe(this) {
+            binding.customToolBar.setAmount(it)
+        }
+    }
     private fun setUpNavigation() {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.navHostFragment) as NavHostFragment
