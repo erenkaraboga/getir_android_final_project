d17812f671a44fb5af0bad3721646e5b4baed063
erenkaraboga <eren.karaboga@naylalabs.com>
2024-04-14 15:32:08 +0300

    feat : create custom basket button and ui

---
+++ core/src/main/java/com/getir/core/common/ui/CustomBasketButton.kt
@@ -0,0 +1,41 @@
+package com.getir.core.common.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.getir.core.R
+import com.google.android.material.card.MaterialCardView
+
+class CustomBasketButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    private val tvAmount: TextView
+    private val root: MaterialCardView
+    init {
+        LayoutInflater.from(context).inflate(R.layout.custom_basket_button, this, true)
+        tvAmount = findViewById(R.id.tvAmount)
+        root = findViewById(R.id.cvBasket)
+    }
+
+    fun setAmount(amount: Int) {
+        val slideAnimation = TranslateAnimation(root.width.toFloat(), 0f, 0f, 0f)
+        slideAnimation.duration = 300
+        root.startAnimation(slideAnimation)
+
+        slideAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                "₺${amount}".also { tvAmount.text = it }
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {}
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+    }
+}
