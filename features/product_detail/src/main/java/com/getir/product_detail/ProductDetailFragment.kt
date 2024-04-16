ae95c19bdd627ae308cb0ecc858b8289b79c8d38
erenkaraboga <eren.karaboga@naylalabs.com>
2024-04-16 23:25:58 +0300

    feat : update product detail fragment for new items

--- features/product_detail/src/main/java/com/getir/product_detail/ProductDetailFragment.kt
+++ features/product_detail/src/main/java/com/getir/product_detail/ProductDetailFragment.kt
@@ -1,44 +1,55 @@
 package com.getir.product_detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.getir.core.SharedViewModel
 import com.getir.core.common.constants.ToolBarType
 import com.getir.core.common.ui.CustomQuantityButtonDetail
+import com.getir.core.domain.models.Product
 import com.getir.product_detail.databinding.FragmentProductDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ProductDetailFragment : Fragment() {
     private val sharedViewModel: SharedViewModel by activityViewModels()
+    private lateinit var product: Product
     private lateinit var binding: FragmentProductDetailBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        binding = FragmentProductDetailBinding.inflate(layoutInflater)
+        binding = FragmentProductDetailBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        observeCartAmount()
+        observeViewModels()
         setListeners()
     }
 
-    private fun observeCartAmount() {
-        sharedViewModel.cartAmount.observe(viewLifecycleOwner) { quantity ->
-            //binding.customQuantityButton.setQuantity(quantity)
-            if (quantity > 0) {
-                showBasketUI()
-            } else {
-                showRegularUI()
+    private fun observeViewModels() {
+        sharedViewModel.cartItems.observe(viewLifecycleOwner) { cartItems ->
+            val existingProduct = cartItems.find { it.id == sharedViewModel.selectedProduct.value?.id }
+            existingProduct?.let {
+                binding.customQuantityButton.setQuantity(it.quantity)
+                if (it.quantity > 0) {
+                    showBasketUI()
+                } else {
+                    showRegularUI()
+                }
+            } ?: showBasketUI()
+        }
+
+        sharedViewModel.selectedProduct.observe(viewLifecycleOwner) { selectedProduct ->
+            selectedProduct?.let {
+                product = it
+                binding.tvProduct.text = it.name
             }
         }
     }
-
 
     private fun showBasketUI() {
         sharedViewModel.setTopBar(ToolBarType.PRODUCT_DETAIL_BASKET)
@@ -54,16 +65,16 @@ class ProductDetailFragment : Fragment() {
 
     private fun setListeners() {
         binding.customQuantityButton.setOnQuantityChangeListener(object : CustomQuantityButtonDetail.OnQuantityChangeListener {
-            override fun onQuantityIncreased() {
-
+            override fun onQuantityIncreased(quantity: Int) {
+                sharedViewModel.addToCart(product)
             }
 
-            override fun onQuantityDecreased() {
-
+            override fun onQuantityDecreased(quantity: Int) {
+                sharedViewModel.removeFromCart(product)
             }
-
         })
-
+        binding.btnNegative.setOnClickListener {
+            sharedViewModel.addToCart(product)
+        }
     }
 }
-
