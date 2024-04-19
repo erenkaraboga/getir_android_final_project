3a8d9fcc840713ecd81a38681134c030eee67c5e
erenkaraboga <eren.karaboga@naylalabs.com>
2024-04-20 00:56:18 +0300

    feat : update navigation animations

---
+++ core/src/main/java/com/getir/core/common/constants/navigation.kt
@@ -0,0 +1,6 @@
+package com.getir.core.common.constants
+
+object NavigationRoute {
+    const val PRODUCT_DETAIL = "android-app://example.google.app/fragment_product_detail"
+    const val BASKET = "android-app://example.google.app/fragment_basket"
+}
